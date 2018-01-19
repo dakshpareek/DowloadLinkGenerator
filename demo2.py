f3c8506b6cecff9a6aeb83b3d5bb2c13e4d84ab4
Daksh Pareek <dakshpareek7@gmail.com>
2018-01-19 19:43:04 +0530

    Update demo2.py

--- demo2.py
+++ demo2.py
@@ -4,7 +4,23 @@ from bs4 import BeautifulSoup
 from PIL import Image
 import urllib, cStringIO
 
+import kivy
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.image import Image
+
 i="https://static.pexels.com/photos/5390/sunset-hands-love-woman.jpg"
-file = cStringIO.StringIO(urllib.urlopen(i).read())
-im = Image.open(file)
-im.show()
+#file = cStringIO.StringIO(urllib.urlopen(i).read())
+#im = Image.open(file)
+#im.show()
+
+
+
+
+
+class MyApp(App):
+
+  def build(self):
+  	return Image(source=i)
+
+MyApp().run()
