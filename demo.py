f88da594273638bc5166e704433dbb98f6695aef
Daksh Pareek <dakshpareek7@gmail.com>
2018-01-31 14:42:37 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,8 +1,17 @@
+#!C:\Python27\python.exe 
+print "Content-type:text/html\r\n\r\n"
+print '''<html>
+<head></head>
+<body>
+'''
+
 import requests
 import ssl
 from bs4 import BeautifulSoup
 from PIL import Image
 import urllib, cStringIO
+import cgi
+
 
 def make_soup(url):
     try:
@@ -11,11 +20,7 @@ def make_soup(url):
         return None
     return BeautifulSoup(html,"lxml")
 
-check=input("enter page number:")
-if check!=1:
-	url="http://extramovies.cc/page/"+str(check)
-else:
-	url = "http://extramovies.cc/"
+url = "http://extramovies.cc/"
 #url2 = raw_input("Enter Link")
 soup = make_soup(url)
 movies = soup.findAll("div", {"class": "thumbnail"})
@@ -28,28 +33,9 @@ for every_movie in movies:
 	all_data.append([title,link])
 
 for i,j in enumerate(all_data):
-	print "ID:",i,j[0]
+	print "<h2>ID:",i,j[0],"</h2>"
 
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
-
-req=input("Enter ID to get Download Link: ")
-soup2=make_soup(all_data[req][1])
-img=soup2.findAll("img",{"class": "alignnone"})
-for j in range(1,len(img)):
-	i=img[j].attrs["src"]
-	#print i
-	file = cStringIO.StringIO(urllib.urlopen(i,context=ctx).read())
-	im = Image.open(file)
-	im.show()
-
-'''
-d_link=soup2.findAll("a",{"class": "buttn blue"})
-f_link='http://extramovies.cc'+str(d_link[0].attrs["href"])
-soup3=make_soup(f_link)
-last_link=soup3.findAll("a")
-final=last_link[len(last_link)-2].attrs["href"]
-
-print final
+print '''
+</body>
+</html>
 '''
